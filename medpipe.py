83c773c4531395177ca2d099c7a6c6402db45a3a
Benjamin Wu <bwu062005@gmail.com>
2025-01-18 13:19:58 -0500

    Updated computer vision
    Finished the computer vision code to track shoulders and to calculate the distance between hands and shoulders to count a pushup.

--- medpipe.py
+++ medpipe.py
@@ -4,6 +4,15 @@ import time
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
+def distanceCalculate (x, y):
+    dist = ((y[0] - x[0])**2 + (y[1] - x[1])**2)**0.5
+    return dist
+
+mp_pose = mp.solutions.pose
+
+pushUpCounter = 0
+
+#hand capture code
 base_options = python.BaseOptions(model_asset_path='hand_landmerker.task')
 options = vision.HandLandmarkOptions(base_options=base_options, num_hands=2)
 
@@ -15,3 +24,33 @@ result = detector.detect(image)
 
 drawn_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
 cv2_imshow(cv2.cvColor(drawn_image, cv2.COLOR_RGB2BGR))
+
+#video capture code
+vid = cv2.VideoCapture(0)
+
+frame_width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+out = cv2.VIdeoWriter({output video here}, cv2.VideoWriter_fourcc({VIDEO HERE}), 20, (frame_width, frame_height))
+while True:
+    ret, frame = cam.read()
+    out.write(frame)
+    cv2.imshow('Camera', frame)
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cam.release()
+out.release()
+cv2.destroyAllWindows()
+
+leftHand = result.multi_handedness[0].classification[0].label
+rightHand = result.multi_handedness[1].classification[0].label
+leftShoulder = result.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
+rightShoulder = result.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
+
+down = False
+if distanceCalculate(rightShoulder, rightHand) <130:
+    down = True
+elif down and distanceCalculate(rightShoulder, rightHand) > 250:
+    down = False
+    pushUpCounter += 1
